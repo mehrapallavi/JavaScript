7cdc2ed399ce6c65b9aa7b06b64eff4f1c5ff858
Pallavi Mehra <117376335+mehrapallavi@users.noreply.github.com>
2024-01-06 09:12:05 +0000

    datatypes done

---
+++ Basics/datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; //treat all code as newer version of js
+
+//alert(3+3) //  not possible coz we are using node.jd not browser
+
+/*data types
+number
+bigint
+boolean
+string
+null
+undefined
+symbol
+
+object
+
+*/
